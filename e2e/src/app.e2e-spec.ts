b4534f3f7e3b558d5d6468435d4b02c44f68bd72
D-LUSiON <l.peikov@gmail.com>
2019-07-09 00:19:53 +0300

    update: Updated resources and made some fixes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to invoices-elan!');
+    expect(page.getTitleText()).toEqual('Welcome to invoices!');
   });
 
   afterEach(async () => {
