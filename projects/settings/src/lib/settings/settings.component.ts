0ba785bb3a667322b0d1923489da03126043caf8
Lubomir Peikov <l.peikov@gmail.com>
2020-04-03 11:58:39 +0300

    add: Settings -> backups path

--- projects/settings/src/lib/settings/settings.component.ts
+++ projects/settings/src/lib/settings/settings.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SettingsService } from '../settings.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { ElectronClientService, TranslationsService } from '@shared';
 
 @Component({
     selector: 'lib-settings',
@@ -24,6 +25,8 @@ export class SettingsComponent implements OnInit, OnDestroy {
 
     constructor(
         private _fb: FormBuilder,
+        private _electron: ElectronClientService,
+        private _translate: TranslationsService,
         private _settingsService: SettingsService,
     ) {
         this._initForm();
@@ -49,24 +52,27 @@ export class SettingsComponent implements OnInit, OnDestroy {
             'general': this._fb.group({
                 'language': this._fb.control(''),
                 'currency_sign': this._fb.control(''),
+                'backup_path': this._fb.control(''),
             }),
             'sender': this._fb.group({
                 'name': this._fb.control(''),
                 'bulstat': this._fb.control(''),
                 'vat': this._fb.control(''),
                 'address': this._fb.control(''),
-                'email': this._fb.control(''),
+                'email': this._fb.control('', [Validators.email]),
                 'password': this._fb.control(''),
             }),
             'accountant_email': this._fb.group({
-                'email': this._fb.control('')
+                'email': this._fb.control('', [Validators.email])
             }),
         });
         this.settings_keys = Object.keys(this.settingsForm.value);
         if (!this.active_setting) this.active_setting = this.settings_keys[0];
         let save_timeout;
         this.settingsForm.valueChanges.subscribe((changes) => {
-            if (this.settingsForm.dirty) {
+            console.log(changes);
+
+            if (this.settingsForm.dirty && this.settingsForm.valid) {
                 this.settings = this.settingsForm.value;
                 if (save_timeout) clearTimeout(save_timeout);
                 save_timeout = setTimeout(() => {
@@ -77,6 +83,22 @@ export class SettingsComponent implements OnInit, OnDestroy {
                 }, 500);
             }
         });
+    }
+
+    async chooseFolder() {
+        const result = await this._electron.remote.dialog.showOpenDialog(this._electron.window, {
+            title: this._translate.translate('Choose a backup folder'),
+            properties: ['openDirectory']
+        });
+        if (!result.canceled) {
+            this.settingsForm.markAsDirty();
+            this.settingsForm.patchValue({
+                'general': {
+                    'backup_path': result.filePaths[0] || ''
+                }
+            });
+        }
+
     }
 
     activateSetting(key: string) {
